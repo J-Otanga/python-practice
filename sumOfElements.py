4a61a90829fb334ac767424a74b3066bde6c5a30
J-Otanga <furahaotanga@gmail.com>
2025-04-26 00:08:07 +0300

    Sum elements in a list

---
+++ sumOfElements.py
@@ -0,0 +1,9 @@
+#Example of a list
+list=[23,38,33,172,190]
+sum = 0
+
+# Iterate through each number in the list and add it to the total
+for i in list:
+    sum += i
+#output sum
+print("The sum is:",sum)
